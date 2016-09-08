0936de91aa5ed5a6271c4c2e09c2d1ee00c40c33
Dennis <dennis.chau@outlook.com>
2016-09-08 16:51:38 -0700

    Setup project; created Student class

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo "hello world";
+
+//            include ('Student.php');
+//            $students = array();
+//            foreach($students as $student)
+//                echo $student->toString();
+//
+//            $first = new Student();
+//            $first->surname = "Doe";
+//            $first->first_name = "John";
+//            $first->add_email('home','john@doe.com');
+//            $first->add_email('work','jdoe@mcdonalds.com');
+//            $first->add_grade(65);
+//            $first->add_grade(75);
+//            $first->add_grade(55);
+//            $students['j123'] = $first;
+        ?>
+    </body>
+</html>
